745c928321f24b37f281a158ffe27af78f82b0cc
leqvinadzeakaki <leqvinadzeakaki@gmail.com>
2025-09-11 23:34:12 +0400

    Update busines.py

--- busines.py
+++ busines.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.environ["hamleti"]  # აუცილებელია session-ისთვის
+app.secret_key = os.environ["SECRET_KEY"]  # აუცილებელია session-ისთვის
 
 # Render-ზე რეკომენდებული cookie-სეტინგები
 app.config.update(
